a88ec1189e7d97897884698d975c956522278a4e
Alex Wayne <alex.wayne@solvenergy.com>
2021-12-18 15:56:26 -0800

    Breathe mode, cloud functions

--- src/AuroraLamp.ino
+++ src/AuroraLamp.ino
@@ -14,17 +14,29 @@ FASTLED_USING_NAMESPACE
 
 CRGB leds[LED_COUNT];
 
-uint8_t brightness = 255;
+int brightness = 255;
 
-float speed = 1;
-uint8_t mode = 0;
+double speed = 1;
+
+// Magic Rainbow
+// Breathe
+String mode = "";
 
 void setup() {
   FastLED.addLeds<LED_TYPE, LED_DAT, LED_CLK, LED_COLOR_ORDER, DATA_RATE_MHZ(20)>(leds, LED_COUNT);
   FastLED.setBrightness(brightness);
 
+  Particle.variable("brightness", brightness);
+  Particle.variable("speed", speed);
+  Particle.variable("mode", mode);
+
   Particle.function("setSpeed_float", setSpeed_float);
+  Particle.function("setBrightness_255", setBrightness_255);
+
   Particle.function("startMagicRainbow", startMagicRainbow);
+  Particle.function("startBreathe", startBreathe);
+
+  startMagicRainbow("");
 }
 
 int setSpeed_float(String arg) {
@@ -32,18 +44,29 @@ int setSpeed_float(String arg) {
   return 0;
 }
 
+int setBrightness_255(String arg) {
+  brightness = arg.toInt();
+  FastLED.setBrightness(brightness);
+  return 0;
+}
+
 int startMagicRainbow(String arg) {
-  mode = 0;
+  mode = "Magic Rainbow";
+  speed = 1;
+  return 0;
+}
+
+int startBreathe(String arg) {
+  mode = "Breathe";
+  speed = 0.15;
   return 0;
 }
 
 void loop() {
   uint32_t now = millis() * speed;
 
-  switch (mode) {
-  case 0:
-    loopMagicRainbow(now); break;
-  }
+  if (mode == "Magic Rainbow") loopMagicRainbow(now);
+  if (mode == "Breathe") loopBreathe(now);
 
   FastLED.show();
 }
@@ -59,6 +82,21 @@ void loopMagicRainbow(uint32_t now) {
       s = map(s, 0, 255, 128, 255);
 
       uint8_t v = getNoise(now * 0.5, x, y);
+
+      setLed(x, y, h, s, v);
+    }
+  }
+}
+
+void loopBreathe(uint32_t now) {
+  for (uint8_t x = 0; x < X; x++) {
+    for (uint8_t y = 0; y < Y; y++) {
+      uint8_t h = now * 0.02;
+      h += y * 12;
+      h += getNoise(now * 0.8, x, y) * 0.5;
+      
+      uint8_t s = getNoise(now * 0.7, x, y);
+      uint8_t v = beatsin8(speed * 60, 0, 255, now, -y * 15);
 
       setLed(x, y, h, s, v);
     }
